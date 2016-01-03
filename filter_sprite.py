feabfd09d2fce179a03a6ce4999fffc94186f4a8
Caio Moreira Gomes <caiogomes@MiniMarvin>
2016-01-02 21:47:48 -0300

    código teste para gerar os sprites cortados, transparências ainda não incluida

---
+++ filter_sprite.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import os
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+#print(filename)
+
+im = cv2.imread(filename)
+
+print(filename)
+raw_input("PRESS ENTER TO CONTINUE")
+
+print(im.shape)
+
+xconst = 49
+yconst = 109
+
+w = im.shape[0]/xconst
+h = im.shape[1]/yconst
+
+ndir = raw_input("Digite o nome do diretorio para salvar os sprites:")
+
+for i in range(0, h):
+	for j in range(0, w):
+		buff = im[j*yconst:(j+1)*yconst, i*xconst:(i+1)*xconst]
+		if not os.path.exists(ndir):
+			os.makedirs(ndir)
+		cv2.imwrite("./"+ndir+"/sprite_"+str(j)+"_"+str(i)+".png", buff)
